cce065c4773a88c509aa5d6102a42dbdca304f5d
unknown <lidanyang1111@gmail.com>
2022-01-13 16:19:24 -0800

    version3

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const server = require("./api/server.js");
 const dotenv = require("dotenv").config();
+const server = require("./api/server.js");
+
 const Port = process.env.Port || 9000
 
 server.listen(Port, () => {
